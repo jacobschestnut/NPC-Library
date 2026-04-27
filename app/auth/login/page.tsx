e4f684f672bc1edd30a1a2005a0d6693f104b3a2
Jacob Chestnut <jacob.chestnut@fbitn.com>
2026-04-27 08:46:24 -0500

    up to date

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,26 +1,46 @@
-'use client'
+import Image from 'next/image';
+import { supabase } from '@/lib/supabase/client';
+import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
 
-import { supabase } from '@/lib/supabase/client'
-import { useState } from 'react'
+export default function LoginForm() {
+  const signIn = async () => {
+    'use server';
+    
+    const headersList = await headers();
+    const origin = headersList.get('origin');
+    // 2. Sign in with GitHub
+    const { error, data } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+      options: {
+        redirectTo: `${origin}/auth/callback`,
+      },
+    });
 
-export default function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+    if (error) {
+      console.log(error);
+    } else {
+      return redirect(data.url);
+    }
 
-  const handleLogin = async () => {
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
-
-    if (error) alert(error.message)
-  }
+    // 3. Redirect to landing page
+  };
 
   return (
-    <div>
-      <input onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleLogin}>Login</button>
-    </div>
-  )
+    <form
+      action={signIn}
+      className="flex-1 flex min-h-screen justify-center items-center"
+    >
+      <button className="hover:bg-gray-800 p-8 rounded-xl">
+        <Image
+          className="mx-auto mb-3"
+          src="/github-mark-white.png"
+          width={100}
+          height={100}
+          alt="GitHub logo"
+        />
+        Sign in with GitHub
+      </button>
+    </form>
+  );
 }
